2875b5f0858658559c04a3685015cd209703783f
Connor Waldoch <cwal37@gmail.com>
2016-05-26 16:09:38 -0400

    Initializing files
    Uploading by hand until I get home.

---
+++ record_graphs.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 26 09:51:41 2016
+
+@author: wal
+"""
+import pdb
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.font_manager import FontProperties
+import matplotlib.patheffects as path_effects
+
+plt.close()
+mpl.rcdefaults()
+mpl.rcParams['figure.figsize'] = 14, 18
+plt.style.use('ggplot')
+mpl.rcParams.update({'font.size': 12})
+
+
+tc_dict = {'Avril LaGreen':['#12D300', '#FF0000'],
+           'SWAg':['#EFEFEF','#FF0000'],
+           'Mediokra':['#38761D', '#FFFF00'],
+           'MAGA':['#999999','#FF9900'],
+           'Cheerwine?':['#CC0000','#FF9900'],
+           'Blue Ridge':['#0000FF','#FF9900'],
+           'ThunderPants':['#000000','#FF0000'],
+           'Victorious Secret':['#FF00FF','#000000'],
+           'US!':['#01D7FF','#FFFF00'],
+           'Vanilla Whoopass':['#FFFFFF','#FF9900'],
+           'deR':['#FF0000','#FFFF00'],
+           'Purple Reign':['#9900FF','#FFFF00'],
+           'Rubber Huckies':['#FFFF00','#38761D'],
+           'Call Me Navy':['#1C4587','#FF9900']}
+
+
+df = pd.read_csv('raw_records.csv')
+df = df.ix[1:4,:]
+
+games = [0,1,2,3]
+
+for x in tc_dict:
+    
+    print x
+    team_df =  df[x]
+    #pdb.set_trace()
+    plt.plot(games, team_df, label = x, color = tc_dict[x][0], linewidth=5,
+             path_effects=[path_effects.SimpleLineShadow(shadow_color = tc_dict[x][1], alpha = 0.5), path_effects.Normal()])
+
+    
+    
+    #print tc_dict[x][0]
+plt.xticks(games, ['Pre-Season', 'Game 1', 'Game 2', 'Game 3'])
+plt.title('2016 Summer League Game-to-Game Point Differential through Week 3')
+plt.ylabel('Points')
+plt.xlabel('Games')
+plt.gcf().subplots_adjust(bottom=0.14)
+
+legend = plt.legend( loc='upper center', bbox_to_anchor=(0.5, -0.06),fancybox=True, shadow=True, ncol=3)
+plt.savefig('g2g_point_diff.png', dpi = 400)
